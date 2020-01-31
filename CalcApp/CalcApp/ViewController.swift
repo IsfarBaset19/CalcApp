6104b46293c09213240de7b6f2270050b814f09f
Isfar Baset <43085165+IsfarBaset19@users.noreply.github.com>
2020-01-31 14:42:49 -0500

    initial setup of calculator app

---
+++ CalcApp/CalcApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Isfar Baset on 1/30/20.
+//  Copyright © 2020 Isfar Baset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var yardField: UITextField!
+    
+    @IBOutlet weak var meterField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calcBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearBtn(_ sender: UIButton) {
+    }
+    
+    
+}
+
