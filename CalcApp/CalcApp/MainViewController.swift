f7420576ff5bb5d3ba8ab44f0fad290854753668
Isfar Baset <baseti@mail.gvsu.edu>
2020-02-06 19:29:02 -0500

    All features are functional

--- CalcApp/CalcApp/MainViewController.swift
+++ CalcApp/CalcApp/MainViewController.swift
@@ -34,35 +34,301 @@ class MainViewController: UIViewController, SettingsViewControllerDelegate {
         self.view.endEditing(true)
     }
     
+    
 
     @IBAction func calcBtn(_ sender: UIButton) {
-       //1. Meter - yard
-       // 2. yard - meter
-       //3. meter - miles
-        //4. miles-meter
-        //5. miles-yard
-        //6. yard-miles
-        //7. miles-miles
-        //8. yard-yard
-       // 9. meter-meter
         
+        let label1: UILabel = fromUnit
+        let label2: UILabel = toUnit
+        let field1: DecimalMinusTextField! = fromVal
+        let field2: DecimalMinusTextField! = toVal
         
-        if let meters = Double(fromVal.text!){
-            if meters > 0 {
-                // two decimal places conversion
-                toVal.text = String(meters * 1.09361)
+        if lengthUnit{
+            
+            // Meters - Yards
+            if label1.text == "Meters"{
+                if label2.text == "Yards"{
+                    
+                    if let meters = Double(field1.text!){
+                    field2.text = String(meters * 1.09361)
+                    }
+                    else{
+                        if let meters = Double(field2.text!){
+                        field1.text = String(meters / 1.09361)
+                        }
+                    }
+                }
             }
+            
+            //Yards - Meters
+            if label1.text == "Yards"{
+                if label2.text == "Meters"{
+                    
+                    if let yards = Double(field1.text!){
+                    field2.text = String(yards / 1.09361)
+                    }
+                    else{
+                        if let yards = Double(field2.text!){
+                        field1.text = String(yards * 1.09361)
+                        }
+                    }
+                }
+            }
+            
+            //Meters - Miles
+            if label1.text == "Meters"{
+                if label2.text == "Miles"{
+                    
+                    if let meters = Double(field1.text!){
+                    field2.text = String(meters * 0.000621371)
+                    }
+                    else{
+                        if let meters = Double(field2.text!){
+                        field1.text = String(meters / 0.000621371)
+                        }
+                    }
+                }
+            }
+            
+            
+            // Miles - Meter
+            if label1.text == "Miles"{
+                if label2.text == "Meters"{
+                    
+                    if let miles = Double(field1.text!){
+                    field2.text = String(miles / 0.000621371)
+                    }
+                    else{
+                        if let miles = Double(field2.text!){
+                        field1.text = String(miles * 0.000621371)
+                        }
+                    }
+                }
+            }
+            
+            // Yards to Miles
+            
+            if label1.text == "Yards"{
+                if label2.text == "Miles"{
+                    
+                    if let yards = Double(field1.text!){
+                    field2.text = String(yards * 0.000568182)
+                    }
+                    else{
+                        if let miles = Double(field2.text!){
+                        field1.text = String(miles / 0.000568182)
+                        }
+                    }
+                }
+            }
+            
+            // Miles to Yards
+            if label1.text == "Miles"{
+                if label2.text == "Yards"{
+                    
+                    if let miles = Double(field1.text!){
+                    field2.text = String(miles * 1760)
+                    }
+                    else{
+                        if let yards = Double(field2.text!){
+                        field1.text = String(yards / 1760)
+                        }
+                    }
+                }
+            }
+            
+            // Miles to Miles
+            if label1.text == "Miles"{
+                if label2.text == "Miles"{
+                    
+                    if let miles = Double(field1.text!){
+                    field2.text = String(miles)
+                    }
+                    else{
+                        if let miles = Double(field2.text!){
+                        field1.text = String(miles)
+                        }
+                    }
+                }
+            }
+            
+            
+            // Meters to meters
+            if label1.text == "Meters"{
+                if label2.text == "Meters"{
+                    
+                    if let meters = Double(field1.text!){
+                    field2.text = String(meters)
+                    }
+                    else{
+                        if let meters = Double(field2.text!){
+                        field1.text = String(meters)
+                        }
+                    }
+                }
+            }
+            
+            // Yards to Yards
+            if label1.text == "Yards"{
+                if label2.text == "Yards"{
+                    
+                    if let yards = Double(field1.text!){
+                    field2.text = String(yards)
+                    }
+                    else{
+                        if let yards = Double(field2.text!){
+                        field1.text = String(yards)
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            
         }
         
-            if let yards = Double(toVal.text!){
-            if yards > 0 {
-                 // two decimal places cpnversion
-                fromVal.text = String(yards / 1.09361)
-                       
-                       }
+        else{
+            
+            // Liters to Gallons
+            if label1.text == "Liters"{
+                if label2.text == "Gallons"{
+                    
+                    if let liters = Double(field1.text!){
+                    field2.text = String(liters * 0.264172)
+                    }
+                    else{
+                        if let gallons = Double(field2.text!){
+                            field1.text = String(gallons / 0.264172)
+                        }
+                    }
+                }
+            }
+            
+            // Gallons to Liters
+            
+            if label1.text == "Gallons"{
+                        if label2.text == "Liters"{
+                            
+                            if let gallons = Double(field1.text!){
+                            field2.text = String(gallons * 3.78541)
+                            }
+                            else{
+                                if let liters = Double(field2.text!){
+                                    field1.text = String(liters / 3.78541)
+                                }
+                            }
+                        }
+                    }
+            
+            // Liters to Quarts
+            if label1.text == "Liters"{
+                if label2.text == "Quarts"{
+                    
+                    if let liters = Double(field1.text!){
+                    field2.text = String(liters * 1.05669)
+                    }
+                    else{
+                        if let quarts = Double(field2.text!){
+                            field1.text = String(quarts / 1.05669)
+                        }
+                    }
+                }
+            }
+            
+            
+            // Quarts to Liters
+            if label1.text == "Quarts"{
+                        if label2.text == "Liters"{
+                            
+                            if let quarts = Double(field1.text!){
+                            field2.text = String(quarts * 0.946353)
+                            }
+                            else{
+                                if let liters = Double(field2.text!){
+                                    field1.text = String(liters / 0.946353)
+                                }
+                            }
+                        }
+                    }
+            
+            // Gallons to Quarts
+            if label1.text == "Gallons"{
+                                 if label2.text == "Quarts"{
+                                     
+                                     if let gallons = Double(field1.text!){
+                                     field2.text = String(gallons * 4)
+                                     }
+                                     else{
+                                         if let quarts = Double(field2.text!){
+                                             field1.text = String(quarts / 4)
+                                         }
+                                     }
+                                 }
+                             }
+            
+            // Quarts to Gallons
+            if label1.text == "Quarts"{
+                                 if label2.text == "Gallons"{
+                                     
+                                     if let quarts = Double(field1.text!){
+                                     field2.text = String(quarts * 0.25)
+                                     }
+                                     else{
+                                         if let gallons = Double(field2.text!){
+                                             field1.text = String(gallons / 0.25)
+                                         }
+                                     }
+                                 }
+                             }
+            
+            // Gallons to gallons
+            if label1.text == "Gallons"{
+                        if label2.text == "Gallons"{
+                                                
+                        if let gallons = Double(field1.text!){
+                        field2.text = String(gallons)
+                      }
+                     else{
+                      if let gallons = Double(field2.text!){
+                     field1.text = String(gallons)
+                  }
+                  }
+             }
+           }
+            
+            // Liters to Liters
+            if label1.text == "Liters"{
+                                            if label2.text == "Liters"{
+                                                
+                                                if let liters = Double(field1.text!){
+                                                field2.text = String(liters)
+                                                }
+                                                else{
+                                                    if let liters = Double(field2.text!){
+                                                        field1.text = String(liters)
+                                                    }
+                                                }
+                                            }
+                                        }
+            
+            // Quarts to quarts
+            if label1.text == "Quarts"{
+                                            if label2.text == "Quarts"{
+                                                
+                                                if let quarts = Double(field1.text!){
+                                                field2.text = String(quarts)
+                                                }
+                                                else{
+                                                    if let quarts = Double(field2.text!){
+                                                        field1.text = String(quarts)
+                                                    }
+                                                }
+                                            }
+                                        }
+       
         }
-  
-        }
+    }
 
     @IBAction func clearBtn(_ sender: UIButton) {
         fromVal.text = ""
@@ -74,9 +340,7 @@ class MainViewController: UIViewController, SettingsViewControllerDelegate {
         
          self.performSegue(withIdentifier: "SegueToSettings", sender: self)
     }
-    
-
-    
+ 
     @IBAction func modeBtn(_ sender: Any) {
         
         if lengthUnit == true {
@@ -114,6 +378,7 @@ class MainViewController: UIViewController, SettingsViewControllerDelegate {
 
         if let dest = segue.destination.children[0] as? SettingsViewController{
               dest.delegate = self
+              dest.isLength = self.lengthUnit
            }
    }
 
